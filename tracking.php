838c42f64d9ed24faa5990eb841df87bee95369d
ShanMartin12 <blueesha@gmail.com>
2021-09-28 20:26:42 +0530

    Fixed Direct URL access

--- tracking.php
+++ tracking.php
@@ -61,38 +61,59 @@ include("./header1.php");
 							 ?>
 					 
                          <div class="row">
-                             <div class="col-md-7 pad-30 wow fadeInLeft" data-wow-offset="50" data-wow-delay=".30s"> 
+                             <!-- Removed certain details like waybill number, consignor's name etc... -->
+                             <!-- <div class="col-md-7 pad-30 wow fadeInLeft" data-wow-offset="50" data-wow-delay=".30s"> 
                                  <div class="prod-info white-clr" style="color:white;">
                                      <ul>
                                          <li>  <span class="title-2" style="color:white;">Waybill Number: </span>  <span class="fs-16"><?php echo $sql['waybillno'];?> </span>  </li>
 										 <li>  <span class="title-2" style="color:white;">Consignor's Name: </span>  <span class="fs-16"><?php echo $sql['consignor_name'];?> </span>  </li>
 										  <li>  <span class="title-2" style="color:white;">Consignee's Name: </span>  <span class="fs-16"><?php echo $sql['consignee_name'];?> </span>  </li>
                                          <li>  <span class="title-2" style="color:white;">Delivery Address: </span>  <span class="fs-16"><?php echo $sql['consignee_add'];?> </span>  </li>
-                                         
                                      </ul>
                                  </div>
-                             </div>
-                             <div class="col-md-5 pad-30 wow fadeInRight" data-wow-offset="50" data-wow-delay=".30s"> 
-                                 <div class="prod-info white-clr" style="color:white;">
+                             </div> -->
+                             <div class="col-md-12 pad-30 wow fadeInLeft" data-wow-offset="50" data-wow-delay=".30s" > 
+                             <center><div class="prod-info white-clr" style="color:white;">
                                      <ul>
-                                        
+                                        <li>  <span class="title-2" style="color:white;">Waybill Number: </span>  <span class="fs-16"><?php echo $sql['waybillno'];?> </span>  </li>
                                         <li>  <span class="title-2" style="color:white;">Booked date: </span>  <span class="fs-16"><?php echo $sql['book_date'];?> </span>  </li>
                                          <li>  <span class="title-2" style="color:white;">Booking status: </span>  <span class="fs-16 theme-clr"><?php 
 										 $stsql=mysqli_fetch_array(mysqli_query($dbConn,"select * from `tbl_courier_track`,book_status where tbl_courier_track.cons_no='".$_POST['waybill']."' and book_status.b_id=tbl_courier_track.bk_status"));
 										 echo $stsql['bname'];?> </span>  </li>
-                                         <li>  <span class="title-2" style="color:white;">weight (kg): </span>  <span class="fs-16"><?php if($sql['actwgt'] !='0' and $sql['actwgt'] !=""){echo $sql['actwgt'];} else {echo $sql['bxpkg']."Kg Per Box";}?> </span>  </li>
-                                         <li>  <span class="title-2" style="color:white;">No of Qty: </span>  <span class="fs-16"><?php echo $sql['qty'];?></span>  </li>
+                                         <!-- <li>  <span class="title-2" style="color:white;">weight (kg): </span>  <span class="fs-16"><?php if($sql['actwgt'] !='0' and $sql['actwgt'] !=""){echo $sql['actwgt'];} else {echo $sql['bxpkg']."Kg Per Box";}?> </span>  </li> -->
+                                         <!-- <li>  <span class="title-2" style="color:white;">No of Qty: </span>  <span class="fs-16"><?php echo $sql['qty'];?></span>  </li> -->
                                      </ul>
-                                 </div>
-                             </div>
+                                 </div></center>
+                             </div> 
                          </div>
                          <div class="progress-wrap">
                              <div class="progress-status">
-                                 <span class="border-left"></span>
-                                 <span class="border-right"></span>
-                                 <span class="dot dot-left wow fadeIn" data-wow-offset="50" data-wow-delay=".40s"></span>
-                                 <span class="themeclr-border wow fadeIn" data-wow-offset="50" data-wow-delay=".50s">   <span class="dot dot-center theme-clr-bg"></span>  </span>
-                                 <span class="dot dot-right wow fadeIn" data-wow-offset="50" data-wow-delay=".60s"></span>
+                                 <!-- changes to progress bar based the delivery status -->
+                                 <?php if($stsql['bname']=="Intransit"){ ?>
+                                    <!-- If consignment is in transit -->
+                                    <span class="border-left"></span>
+                                    <span class="border-right"></span>
+                                    <span class="dot dot-left wow fadeIn" data-wow-offset="50" data-wow-delay=".40s"></span>
+                                    <span class="themeclr-border wow fadeIn" data-wow-offset="50" data-wow-delay=".50s">   <span class="dot dot-center theme-clr-bg"></span>  </span>
+                                    <span class="dot dot-right wow fadeIn" data-wow-offset="50" data-wow-delay=".60s"></span>
+                                 
+                                 <?php } 
+
+                                    elseif($stsql['bname']=="Delivered") {?>
+                                    <!-- if consignment is delivered -->
+                                        <span class="border-full"></span>
+                                        <span class="dot dot-left wow fadeIn" data-wow-offset="50" data-wow-delay=".40s"></span>           
+                                        <span class="dot dot-delivered wow fadeIn"><span class="themeclr-border wow fadeIn" data-wow-offset="50" data-wow-delay=".50s"><span class="dot dot-center theme-clr-bg"></span></span></span>
+
+                                 <?php } 
+
+                                    else{ ?>
+                                        <!--if consignment is about to start-->
+                                        <div class="col-md-8 col-xs-8 col-sm-4 text-center " style="left: 200px; top: 30px">
+                                        <h2 class="title-1 no-margin "> <?php echo "Shipment will start shortly.";?> </h2> 
+                                        </div>
+
+                                    <?php }?>
                              </div>
                              <div class="row progress-content upper-text">
                                  <div class="col-md-3 col-xs-8 col-sm-2">
@@ -104,15 +125,22 @@ include("./header1.php");
 									 ?> 
 									 </h2>
                                  </div>
-                                 <div class="col-md-2 col-xs-8 col-sm-3">
+                                 <!-- <div class="col-md-2 col-xs-8 col-sm-3">
                                      <p class="fs-12 no-margin"> [  <b class="black-clr"><?php echo 'Booked On: '.$sql['book_date'];?> </b> ]  </p>                                
-                                 </div>
-                                 <div class="col-md-4 col-xs-8 col-sm-4 text-center">
+                                 </div> -->
+                                 <?php if($stsql['bname']=="Intransit"){ ?>
+                                 <div class="col-md-8 col-xs-8 col-sm-4 text-center">
                                      <p class="fs-12 no-margin"> currently in  </p>
-                                     <h2 class="title-1 no-margin"><?php $offsql=mysqli_fetch_array(mysqli_query($dbConn,"select * from `city`,`tbl_offices` where tbl_offices.id='".$stsql['current_city']."' and city.b_id=tbl_offices.city and tbl_offices.status='A'"));
-										 echo $offsql['bname']; ?> </h2>
+                                     <h2 class="title-1 no-margin">
+                                         <?php
+                                            // if ($stsql['bname']=="Intransit"){
+                                                echo $stsql['bname'];
+                                            
+                                         ?>
+                                     </h2>
                                  </div>
-                                 <div class="col-md-1 col-xs-8 col-sm-1 no-pad">
+                                 <?php }?>
+                                 <!-- <div class="col-md-1 col-xs-8 col-sm-1 no-pad">
                                      <p class="fs-12 no-margin"> [  <b class="black-clr"><?php 
 									 if($stsql['bk_status'] !='1')
 									 {
@@ -123,9 +151,9 @@ include("./header1.php");
 										 echo "Shipment will start shortly.";
 									 }
 									?>  </b> ]  </p>                                
-                                 </div>
-                                 <div class="col-md-2 col-xs-8 col-sm-2 text-right">
-                                     <p class="fs-12 no-margin"> to  </p>
+                                 </div> -->
+                                 <div class="col-md-12 col-xs-8 col-sm-2 text-right" style="bottom: 38px;">
+                                     <p class="fs-12 no-margin">  TO </p>
                                      <h2 class="title-1 no-margin"><?php $offsql=mysqli_fetch_array(mysqli_query($dbConn,"select * from `city`,`tbl_offices` where tbl_offices.id='".$sql['dest_off']."' and city.b_id=tbl_offices.city and tbl_offices.status='A'"));
 										 echo $offsql['bname']; ?>  </h2>
                                  </div>
@@ -136,13 +164,11 @@ include("./header1.php");
 							 else
 							 {
 							 ?>
-							                          <div class="row">
+							<div class="row">
                              <div class="col-md-12 pad-30 wow fadeInLeft" data-wow-offset="50" data-wow-delay=".30s"> 
-                                 <div class="prod-info white-clr">
+                                 <div class="prod-info blac-clr">
                                      <ul>
-                                         <li>  <center>Waybill Number Does Not exist. Please Try with Proper Waybill Numbers</center>  </li>
-										 
-                                         
+                                         <li>  <center style="color: red; font-weight:bolder; font-size:large;">Waybill Number Does Not exist. Please Try with Proper Waybill Numbers</center>  </li>
                                      </ul>
                                  </div>
                              </div>
