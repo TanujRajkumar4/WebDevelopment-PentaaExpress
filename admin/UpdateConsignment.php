8c23b6f5b40fe3093e77c507f8d72d568208755b
TANUJ RAJKUMAR <medicustanuj@gmail.com>
2021-10-02 22:17:55 +0530

    Bulk Updates for Consignment - Current City Update - Tanuj

--- admin/UpdateConsignment.php
+++ admin/UpdateConsignment.php
@@ -5,21 +5,27 @@ if ((isset($_SESSION)) && (isset($_SESSION['uid'])) && isset($_POST["UpdateConsi
     $id = $_SESSION['uid'];
     $ConsignmentStatus = $_POST['Book_Status_Update'];
     $SQL = "UPDATE tbl_courier_track SET bk_status = $ConsignmentStatus WHERE cons_no IN ( ";
+    $SQL1 = "UPDATE tbl_courier_track SET current_city = $id WHERE cons_no IN ( ";
 
     $SelectedWayBillNos[] = $_POST['SelectedWayBill'];
     foreach ($SelectedWayBillNos as $WayBillNo) {
         foreach ($WayBillNo as $WayBillNo1) {
 
             $SQL = $SQL . $WayBillNo1 . " , ";
+            $SQL1 = $SQL1 . $WayBillNo1 . " , ";
         }
     }
     $SQL = rtrim($SQL, " , ");
+    $SQL1 = rtrim($SQL1, " , ");
     $SQL = $SQL . " )";
+    $SQL1 = $SQL1 . " )";
     // echo $SQL;
 
     $sql = mysqli_query($dbConn, $SQL);
+    $sql1 = mysqli_query($dbConn, $SQL1);
     echo $sql;
-    if ($sql) {
+    echo $sql1;
+    if ($sql && $sql1) {
         echo "<script>alert('Updated Successfully');window.location.href = 'view-consignment.php';</script>";
     } else {
         echo "<script>alert('Not Updated');window.location.href = 'view-consignment.php';</script>";
