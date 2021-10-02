9d55ba971ad6d17e0874ef720f4cc21961d8e642
TANUJ RAJKUMAR <medicustanuj@gmail.com>
2021-10-02 22:23:35 +0530

    Customer Tariff Initial Release - Tanuj

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,261 +1,274 @@
 <?php
-	require_once('database.php');
-	$type=$_SESSION['type'];
+require_once('database.php');
+$type = $_SESSION['type'];
 ?>
 
 <ul class="nav nav-list">
-					<li class="">
-						<a href="dashboard.php">
-							<i class="menu-icon fa fa-tachometer"></i>
-							<span class="menu-text"> Dashboard </span>
-						</a>
+	<li class="">
+		<a href="dashboard.php">
+			<i class="menu-icon fa fa-tachometer"></i>
+			<span class="menu-text"> Dashboard </span>
+		</a>
 
-						<b class="arrow"></b>
-					</li>
-					<?php if (($type== "HO")) { ?>
-					<li class="">
-						<a href="page.php">
-							<i class="menu-icon fa fa-tachometer"></i>
-							<span class="menu-text"> About Us </span>
-						</a>
+		<b class="arrow"></b>
+	</li>
+	<?php if (($type == "HO")) { ?>
+		<li class="">
+			<a href="page.php">
+				<i class="menu-icon fa fa-tachometer"></i>
+				<span class="menu-text"> About Us </span>
+			</a>
 
-						<b class="arrow"></b>
-					</li>
-					
-					<li class="">
-						<a href="#" class="dropdown-toggle">
-							<i class="menu-icon fa fa-desktop"></i>
-							<span class="menu-text">
-								Master
-							</span>
+			<b class="arrow"></b>
+		</li>
 
-							<b class="arrow fa fa-angle-down"></b>
-						</a>
+		<li class="">
+			<a href="#" class="dropdown-toggle">
+				<i class="menu-icon fa fa-desktop"></i>
+				<span class="menu-text">
+					Master
+				</span>
 
-						<b class="arrow"></b>
+				<b class="arrow fa fa-angle-down"></b>
+			</a>
 
-						<ul class="submenu">
-							<li class="">
-								<a href="add-master.php?ty=ts&ac=ins" >
-									<i class="menu-icon fa fa-caret-right"></i>
+			<b class="arrow"></b>
 
-									Type of Consignment
-									
-								</a>
-
-								<b class="arrow"></b>
-
-							</li>
-							<li class="">
-								<a href="add-master.php?ty=bm&ac=ins" >
-									<i class="menu-icon fa fa-caret-right"></i>
-
-									Units
-									
-								</a>
-
-								<b class="arrow"></b>
-
-							</li>
-							<li class="">
-								<a href="add-master.php?ty=pm&ac=ins" >
-									<i class="menu-icon fa fa-caret-right"></i>
-
-									Payment Mode
-									
-								</a>
-
-								<b class="arrow"></b>
-
-							</li>
-							<li class="">
-								<a href="add-master.php?ty=pin&ac=ins" >
-									<i class="menu-icon fa fa-caret-right"></i>
-
-									Pincode
-									
-								</a>
-
-								<b class="arrow"></b>
-
-							</li>
-							<li class="">
-								<a href="add-master.php?ty=s&ac=ins" >
-									<i class="menu-icon fa fa-caret-right"></i>
-
-									Booking Status
-									
-								</a>
-
-								<b class="arrow"></b>
-
-							</li>
-							
-							<li class="">
-								<a href="add-master.php?ty=cty&ac=ins" >
-									<i class="menu-icon fa fa-caret-right"></i>
-
-									City
-									
-								</a>
-
-								<b class="arrow"></b>
-
-							</li>
-							<li class="">
-								<a href="tariff.php?ty=add" >
-									<i class="menu-icon fa fa-caret-right"></i>
-
-									Tariff
-									
-								</a>
-
-								<b class="arrow"></b>
-
-							</li>
-							</ul>
-					</li>
-					<?php } ?>
-					<li class="">
-						<a href="#" class="dropdown-toggle">
-							<i class="menu-icon fa fa-desktop"></i>
-							<span class="menu-text">
-								Consignment
-							</span>
-
-							<b class="arrow fa fa-angle-down"></b>
-						</a>
-
-						<b class="arrow"></b>
-
-						<ul class="submenu">
-							<li class="">
-								<a href="add-consignment.php?ty=add" >
-									<i class="menu-icon fa fa-caret-right"></i>
-
-									Add Consignment
-									
-								</a>
-
-								<b class="arrow"></b>
-
-							</li>
-							<li class="">
-								<a href="view-consignment.php" >
-									<i class="menu-icon fa fa-caret-right"></i>
-
-									Update Consignment
-									
-								</a>
-
-								<b class="arrow"></b>
-
-							</li>
-							</ul>
-					</li>
-					<li class="">
-						<a href="#" class="dropdown-toggle">
-							<i class="menu-icon fa fa-list-alt"></i>
-							<span class="menu-text">
-								Customer
-							</span>
-
-							<b class="arrow fa fa-angle-down"></b>
-						</a>
-
-						<b class="arrow"></b>
-
-						<ul class="submenu">
-							<li class="">
-								<a href="add-customer.php?ty=add" >
-									<i class="menu-icon fa fa-caret-right"></i>
-
-									Add Customer
-									
-								</a>
-
-								<b class="arrow"></b>
-
-							</li>
-
-							<li class="">
-								<a href="history.php">
-									<i class="menu-icon fa fa-caret-right"></i>
-									Customer History
-								</a>
-
-								<b class="arrow"></b>
-							</li>
-							</ul>
-					</li>
-					<?php if (($type== "HO")) { ?>
-						<li class="">
-					<a href="#" class="dropdown-toggle">
-							<i class="menu-icon fa fa-picture-o"></i>
-							<span class="menu-text"> Branch & User Mgt </span>
-							<b class="arrow fa fa-angle-down"></b>
-						</a>
-
-						<b class="arrow"></b>
-							<ul class="submenu">
+			<ul class="submenu">
 				<li class="">
-						<a href="add-branch.php?ty=add">
-							<i class="menu-icon fa fa-caret-right"></i>
-							<span class="menu-text"> Branch </span>
-						</a>
+					<a href="add-master.php?ty=ts&ac=ins">
+						<i class="menu-icon fa fa-caret-right"></i>
 
-						<b class="arrow"></b>
-					</li>
+						Type of Consignment
+
+					</a>
+
+					<b class="arrow"></b>
+
+				</li>
 				<li class="">
-						<a href="add-branch-user.php?ty=add">
-							<i class="menu-icon fa fa-caret-right"></i>
-							<span class="menu-text"> User </span>
-						</a>
+					<a href="add-master.php?ty=bm&ac=ins">
+						<i class="menu-icon fa fa-caret-right"></i>
 
-						<b class="arrow"></b>
-					</li>
-					
-					</ul></li>
+						Units
+
+					</a>
+
+					<b class="arrow"></b>
+
+				</li>
 				<li class="">
-						<a href="trip-sheet.php">
-							<i class="menu-icon fa fa-picture-o"></i>
-							<span class="menu-text"> Trip Sheet </span>
-						</a>
+					<a href="add-master.php?ty=pm&ac=ins">
+						<i class="menu-icon fa fa-caret-right"></i>
 
-						<b class="arrow"></b>
-					</li>
-					<?php } ?>
-					<li class="">
-						<a href="#" class="dropdown-toggle">
-							<i class="menu-icon fa fa-picture-o"></i>
-							<span class="menu-text"> Report </span>
-							<b class="arrow fa fa-angle-down"></b>
-						</a>
+						Payment Mode
 
-						<b class="arrow"></b>
-							<ul class="submenu">
-							<li class="">
-								<a href="report.php" >
-									<i class="menu-icon fa fa-caret-right"></i>
+					</a>
 
-									Consignment Report
-									
-								</a>
+					<b class="arrow"></b>
 
-								<b class="arrow"></b>
+				</li>
+				<li class="">
+					<a href="add-master.php?ty=pin&ac=ins">
+						<i class="menu-icon fa fa-caret-right"></i>
 
-							</li>
-							<li class="">
-								<a href="cust-report.php" >
-									<i class="menu-icon fa fa-caret-right"></i>
+						Pincode
 
-									Customer Report
-									
-								</a>
+					</a>
 
-								<b class="arrow"></b>
+					<b class="arrow"></b>
 
-							</li>
-							</ul>
-					</li> 
-					</ul>	
+				</li>
+				<li class="">
+					<a href="add-master.php?ty=s&ac=ins">
+						<i class="menu-icon fa fa-caret-right"></i>
+
+						Booking Status
+
+					</a>
+
+					<b class="arrow"></b>
+
+				</li>
+
+				<li class="">
+					<a href="add-master.php?ty=cty&ac=ins">
+						<i class="menu-icon fa fa-caret-right"></i>
+
+						City
+
+					</a>
+
+					<b class="arrow"></b>
+
+				</li>
+				<li class="">
+					<a href="tariff.php?ty=add">
+						<i class="menu-icon fa fa-caret-right"></i>
+
+						Tariff
+
+					</a>
+
+					<b class="arrow"></b>
+
+				</li>
+
+				<li class="">
+					<a href="customer_tariff.php?ty=add">
+						<i class="menu-icon fa fa-caret-right"></i>
+
+						Customer - Tariff
+
+					</a>
+
+					<b class="arrow"></b>
+
+				</li>
+			</ul>
+		</li>
+	<?php } ?>
+	<li class="">
+		<a href="#" class="dropdown-toggle">
+			<i class="menu-icon fa fa-desktop"></i>
+			<span class="menu-text">
+				Consignment
+			</span>
+
+			<b class="arrow fa fa-angle-down"></b>
+		</a>
+
+		<b class="arrow"></b>
+
+		<ul class="submenu">
+			<li class="">
+				<a href="add-consignment.php?ty=add">
+					<i class="menu-icon fa fa-caret-right"></i>
+
+					Add Consignment
+
+				</a>
+
+				<b class="arrow"></b>
+
+			</li>
+			<li class="">
+				<a href="view-consignment.php">
+					<i class="menu-icon fa fa-caret-right"></i>
+
+					Update Consignment
+
+				</a>
+
+				<b class="arrow"></b>
+
+			</li>
+		</ul>
+	</li>
+	<li class="">
+		<a href="#" class="dropdown-toggle">
+			<i class="menu-icon fa fa-list-alt"></i>
+			<span class="menu-text">
+				Customer
+			</span>
+
+			<b class="arrow fa fa-angle-down"></b>
+		</a>
+
+		<b class="arrow"></b>
+
+		<ul class="submenu">
+			<li class="">
+				<a href="add-customer.php?ty=add">
+					<i class="menu-icon fa fa-caret-right"></i>
+
+					Add Customer
+
+				</a>
+
+				<b class="arrow"></b>
+
+			</li>
+
+			<li class="">
+				<a href="history.php">
+					<i class="menu-icon fa fa-caret-right"></i>
+					Customer History
+				</a>
+
+				<b class="arrow"></b>
+			</li>
+		</ul>
+	</li>
+	<?php if (($type == "HO")) { ?>
+		<li class="">
+			<a href="#" class="dropdown-toggle">
+				<i class="menu-icon fa fa-picture-o"></i>
+				<span class="menu-text"> Branch & User Mgt </span>
+				<b class="arrow fa fa-angle-down"></b>
+			</a>
+
+			<b class="arrow"></b>
+			<ul class="submenu">
+				<li class="">
+					<a href="add-branch.php?ty=add">
+						<i class="menu-icon fa fa-caret-right"></i>
+						<span class="menu-text"> Branch </span>
+					</a>
+
+					<b class="arrow"></b>
+				</li>
+				<li class="">
+					<a href="add-branch-user.php?ty=add">
+						<i class="menu-icon fa fa-caret-right"></i>
+						<span class="menu-text"> User </span>
+					</a>
+
+					<b class="arrow"></b>
+				</li>
+
+			</ul>
+		</li>
+		<li class="">
+			<a href="trip-sheet.php">
+				<i class="menu-icon fa fa-picture-o"></i>
+				<span class="menu-text"> Trip Sheet </span>
+			</a>
+
+			<b class="arrow"></b>
+		</li>
+	<?php } ?>
+	<li class="">
+		<a href="#" class="dropdown-toggle">
+			<i class="menu-icon fa fa-picture-o"></i>
+			<span class="menu-text"> Report </span>
+			<b class="arrow fa fa-angle-down"></b>
+		</a>
+
+		<b class="arrow"></b>
+		<ul class="submenu">
+			<li class="">
+				<a href="report.php">
+					<i class="menu-icon fa fa-caret-right"></i>
+
+					Consignment Report
+
+				</a>
+
+				<b class="arrow"></b>
+
+			</li>
+			<li class="">
+				<a href="cust-report.php">
+					<i class="menu-icon fa fa-caret-right"></i>
+
+					Customer Report
+
+				</a>
+
+				<b class="arrow"></b>
+
+			</li>
+		</ul>
+	</li>
+</ul>
