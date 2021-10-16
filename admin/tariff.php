0fdf5cc304052a2c3aef3470ec0dcf40952c8510
ShanMartin12 <blueesha@gmail.com>
2021-10-16 17:23:30 +0530

    footer fix- shajahan

--- admin/tariff.php
+++ admin/tariff.php
@@ -463,7 +463,13 @@ if ((isset($_SESSION)) && (isset($_SESSION['uid']))) {
 					}
 					?>
 				</div>
-				<div class="footer">
+				
+
+				<a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
+					<i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
+				</a>
+			</div><!-- /.main-container -->
+			<div class="footer">
 					<div class="footer-inner">
 						<div class="footer-content">
 							<span class="bigger-120">
@@ -476,12 +482,6 @@ if ((isset($_SESSION)) && (isset($_SESSION['uid']))) {
 						</div>
 					</div>
 				</div>
-
-				<a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
-					<i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
-				</a>
-			</div><!-- /.main-container -->
-
 			<!-- basic scripts -->
 
 			<!--[if !IE]> -->
